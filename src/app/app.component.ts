d3b91f4aff481abd131e4801bfa51dd27fb79e00
sandeep singh <sandeep.gwebs@gmail.com>
2019-05-13 19:48:38 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    {
+      title: 'Home',
+      url: '/home',
+      icon: ''
+    },
+    {
+      title: 'Profile',
+      url: '/profile',
+      icon: ''
+    },
+	{
+      title: 'Notification',
+      url: '/list',
+      icon: '2'
+    },
+	{
+      title: 'Payment',
+      url: '/list',
+      icon: ''
+    },
+	{
+      title: 'Feedback',
+      url: '/list',
+      icon: ''
+    },
+	{
+      title: 'Invite',
+      url: '/list',
+      icon: ''
+    },
+	{
+      title: 'Doctors',
+      url: '/list',
+      icon: ''
+    }
+	
+	
+  ];
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+}
---
+++ src/app/list/list.page.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+@Component({
+  selector: 'app-list',
+  templateUrl: 'list.page.html',
+  styleUrls: ['list.page.scss']
+})
+export class ListPage implements OnInit {
+  private selectedItem: any;
+  private icons = [
+    'flask',
+    'wifi',
+    'beer',
+    'football',
+    'basketball',
+    'paper-plane',
+    'american-football',
+    'boat',
+    'bluetooth',
+    'build'
+  ];
+  public items: Array<{ title: string; note: string; icon: string }> = [];
+  constructor(private camera: Camera) {
+    for (let i = 1; i < 11; i++) {
+      this.items.push({
+        title: 'Item ' + i,
+        note: 'This is item #' + i,
+        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      });
+    }
+  }
+  currentImage: any;
+  
+   ngOnInit() {
+  }
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.currentImage = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+     console.log("Camera issue:" + err);
+    });
+  }
+  // add back when alpha.4 is out
+  // navigate(item) {
+  //   this.router.navigate(['/list', JSON.stringify(item)]);
+  // }
+}
